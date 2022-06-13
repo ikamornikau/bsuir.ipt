7433c43f8a4e4766fe5cc86006690f5f8c47e6d0
Ilya Kamornikov <ilya.kamornikov@scorum.com>
2022-06-13 18:37:54 +0300

    Add setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name="serializer",
+   version="1.0",
+   description="Serializer and CLI tool",
+   author="Ilya Kamornikau",
+   author_email="mr.kamornikov@gmail.com",
+   packages=['serializer'],
+)
